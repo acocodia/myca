01037fd0a8467155c41f71e3041a1cf070a23c36
Alex Cocodia <coco@Alexs-MacBook-Pro.local>
2025-11-04 16:02:37 +0000

    Arsenal Webpage

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 
-resource "aws_security_group" "zander_security_group" {
+resource "aws_security_group" "alexander_security_group" {
      ingress {
     from_port = 80
     to_port = 80
@@ -30,25 +30,25 @@ resource "aws_security_group" "zander_security_group" {
         cidr_blocks = ["0.0.0.0/0"]//allow all outgoing traffic
     } 
 }
-resource "aws_key_pair" "zander_key" {
-    key_name = "zander_key"
-    public_key = file("zander.pub")
+resource "aws_key_pair" "alexander_key" {
+    key_name = "alexander_key"
+    public_key = file("alexander.pub")
   
 }
-resource "aws_instance" "zander_server" {
+resource "aws_instance" "alexander_server" {
     ami = "ami-0c5204531f799e0c6"
     instance_type = "t3.micro"
-    security_groups = [aws_security_group.zander_security_group.name]
-    key_name = aws_key_pair.zander_key.key_name
+    security_groups = [aws_security_group.alexander_security_group.name]
+    key_name = aws_key_pair.alexander_key.key_name
     tags = {
-        Name = "zander_server"
+        Name = "alexander_server"
     }
     
     provisioner "local-exec" {
-        command = "sleep 30 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${self.public_ip},' -u ec2-user --private-key zander deploy.yml"
+        command = "sleep 30 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${self.public_ip},' -u ec2-user --private-key alexander deploy.yml"
     }
 }
 output "instance_public_ip" {
-    value = aws_instance.zander_server.public_ip
+    value = aws_instance.alexander_server.public_ip
   
 }
